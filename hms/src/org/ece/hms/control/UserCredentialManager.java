657cc8b1ee79cc1e1958798ba6452ee6b4df2a91
Andrei Tulai <andrei@andrei-MSI-ubuntu.(none)>
2012-03-29 17:49:00 -0400

    registration and session management

---
+++ hms/src/org/ece/hms/control/UserCredentialManager.java
@@ -0,0 +1,49 @@
+package org.ece.hms.control;
+
+import org.ece.hms.data.UserDAO;
+import org.ece.hms.model.User;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.Sessions;
+
+public class UserCredentialManager {
+	 
+    private static final String KEY_USER_MODEL = UserCredentialManager.class.getName()+"_MODEL";
+    private UserDAO userDAO;
+    private User user;
+     
+    private UserCredentialManager(){
+        userDAO = new UserDAO();
+    }
+     
+    public static UserCredentialManager getIntance() {
+        return getIntance(Sessions.getCurrent());
+    }
+    /**
+     * 
+     * @return
+     */
+    public static UserCredentialManager getIntance(Session zkSession){
+        synchronized(zkSession){
+            UserCredentialManager userModel = (UserCredentialManager) zkSession.getAttribute(KEY_USER_MODEL);
+            if(userModel==null){
+                zkSession.setAttribute(KEY_USER_MODEL, userModel = new UserCredentialManager());
+            }
+            return userModel;
+        }
+    }
+    
+    public void authenticate(User user) {
+    	this.user = user;
+    }
+    
+    public boolean isAuthenticated() {
+    	if (user != null) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public User getUser() {
+    	return this.user;
+    }
+}
