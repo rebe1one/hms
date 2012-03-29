1f3eeffb0b8c0b725dbfdf332d4079b0f948314e
Kanish Raajkumar <kanish89@gmail.com>
2012-03-29 12:07:04 -0400

    Added DAOs for real this time

---
+++ hms/src/org/ece/hms/data/PatientDAO.java
@@ -0,0 +1,68 @@
+package org.ece.hms.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ece.hms.model.Patient;
+
+public class PatientDAO extends DAO implements iDAO<Patient> {
+	protected final DataSource ds = DataSource.INSTANCE;
+
+	@Override
+	public List<Patient> findAll() {
+		List<Patient> allPatients = new ArrayList<Patient>();
+		try {
+			// get connection
+		    Statement stmt = ds.getStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Patient");
+
+			// fetch all events from database
+			Patient patient;
+			
+			while (rs.next()) {
+				patient = new Patient();
+				patient.setUserId(rs.getInt(1));
+				patient.setAddress(rs.getString(2));
+				patient.setProvince(rs.getString(3));
+				patient.setSIN(rs.getInt(4));
+				patient.setHealthCardNumber(rs.getInt(5));
+				patient.setPhoneNumber(rs.getInt(6));
+				patient.setCurrentHealth(rs.getString(7));
+				allPatients.add(patient);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+		    ds.close();
+		}
+		
+		return allPatients;
+	}
+	
+	@Override
+	public boolean delete(Patient entity) {
+		return execute("DELETE FROM Patient WHERE user_id = '" + entity.getUserId() + "'");
+	}
+
+	@Override
+	public Boolean insert(Patient entity) {
+		return execute("INSERT INTO Patient(user_id,address,province,SIN,health_card_number,phone_number,current_health) " +
+                "VALUES (" + entity.getUserId() + ",'" + entity.getAddress() +
+                "','" + entity.getProvince() + entity.getSIN() + entity.getHealthCardNumber() + 
+                entity.getPhoneNumber() + entity.getCurrentHealth() + "')");
+	}
+
+	@Override
+	public boolean update(Patient entity) {
+		return execute("UPDATE Patient SET address = '" + entity.getAddress() + 
+                "', province = '" + entity.getProvince() + "', SIN = '" + entity.getSIN() +
+                "', health_card_number = '" + entity.getHealthCardNumber() + 
+                "', phone_number = '" + entity.getPhoneNumber() +
+                "', current_health = '" + entity.getCurrentHealth() + 
+                "' where user_id = " + entity.getUserId());
+	}
+
+}
---
+++ hms/src/org/ece/hms/data/VisitDAO.java
@@ -0,0 +1,73 @@
+package org.ece.hms.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ece.hms.model.Visit;
+
+public class VisitDAO extends DAO implements iDAO<Visit> {
+	protected final DataSource ds = DataSource.INSTANCE;
+
+	@Override
+	public List<Visit> findAll() {
+		List<Visit> allVisits = new ArrayList<Visit>();
+		try {
+			// get connection
+		    Statement stmt = ds.getStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Visit");
+
+			// fetch all events from database
+			Visit visit;
+			
+			while (rs.next()) {
+				visit = new Visit();
+				visit.setId(rs.getInt(1));
+				visit.setAppointmentId(rs.getInt(2));
+				visit.setLength(rs.getInt(3));
+				visit.setDiagnosis(rs.getString(4));
+				visit.setPrescription(rs.getString(5));
+				visit.setScheduling(rs.getString(6));
+				visit.setComments(rs.getString(7));
+				visit.setTimestamp(rs.getTimestamp(8));
+				visit.setCreatedBy(rs.getInt(9));
+				allVisits.add(visit);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+		    ds.close();
+		}
+		
+		return allVisits;
+	}
+	
+	@Override
+	public boolean delete(Visit entity) {
+		return execute("DELETE FROM Visit WHERE id = '" + entity.getId() + "'");
+	}
+
+	@Override
+	public Boolean insert(Visit entity) {
+		return execute("INSERT INTO Visit(id,appointment_id,length,diagnosis,prescription,scheduling,comments,timestamp,created_by) " +
+                "VALUES (" + entity.getId() + ",'" + entity.getAppointmentId() +
+                "','" + entity.getLength() + entity.getDiagnosis() + entity.getPrescription() + 
+                entity.getScheduling() + entity.getComments() + entity.getTimestamp() + 
+                entity.getCreatedBy() + "')");
+	}
+
+	@Override
+	public boolean update(Visit entity) {
+		return execute("UPDATE Visit SET appointment_id = '" + entity.getAppointmentId() + 
+                "', length = '" + entity.getLength() + "', diagnosis = '" + entity.getDiagnosis() +
+                "', prescription = '" + entity.getPrescription() + 
+                "', scheduling = '" + entity.getScheduling() +
+                "', comments = '" + entity.getComments() +
+                "', timestamp = '" + entity.getTimestamp() + 
+                "', created_by = '" + entity.getCreatedBy() + 
+                "' where id = " + entity.getId());
+	}
+
+}
