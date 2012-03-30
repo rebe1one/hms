ef2571e8c13f81824d1e02759b318dd41b429af5
Andrei Tulai <andrei@andrei-MSI-ubuntu.(none)>
2012-03-30 15:35:09 -0400

    modal stuff

--- hms/src/org/ece/hms/control/ModalLookupController.java
+++ hms/src/org/ece/hms/control/ModalLookupController.java
@@ -1,17 +1,10 @@
 package org.ece.hms.control;
 
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 
 import org.ece.hms.data.UserDAO;
-import org.ece.hms.data.VisitDAO;
 import org.ece.hms.model.User;
-import org.ece.hms.model.Visit;
-import org.ece.hms.util.Util;
-import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
-import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Grid;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.ListModelList;
@@ -19,18 +12,22 @@ import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
 
-public class ModalLookupController extends SelectorComposer<Window> {
-	@Wire
+public class ModalLookupController extends GenericForwardComposer<Window> {
     private Textbox nameTxb;
-	
-    @Wire
     private Label mesgLbl;
-    
-    @Wire
     private Grid results;
     
-    @Listen("onClick=#confirmBtn")
-    public void search() {
+    @Override
+	public void doAfterCompose(Window window) throws Exception {
+		super.doAfterCompose(window);
+
+		if (arg.containsKey("textbox")) {
+			Textbox box = (Textbox)arg.get("textbox");
+			box.setValue("lol");
+		}
+    }
+    
+    public void onClick$confirmBtn() {
     	UserDAO userDAO = new UserDAO();
     	List<User> users = userDAO.findAll();
     	results.setModel(new ListModelList<User>(users));
