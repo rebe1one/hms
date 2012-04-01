133ac12916f096b658d63046503d598526078d37
Kanish Raajkumar <kanish89@gmail.com>
2012-03-31 20:24:02 -0400

    Added default doctor to patient info

--- hms/src/org/ece/hms/data/DoctorPatientViewDAO.java
+++ hms/src/org/ece/hms/data/DoctorPatientViewDAO.java
@@ -7,9 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.ece.hms.model.Doctor;
 import org.ece.hms.model.DoctorPatientView;
 import org.ece.hms.util.Filter;
 import org.ece.hms.util.Util;
+import org.ece.hms.model.User;
+import org.ece.hms.data.UserDAO;;
 
 public class DoctorPatientViewDAO extends DAO {
 	private String sql = "SELECT relationships.from_id as doctor_id, patient_user.id as patient_id, patient_user.first_name, patient_user.last_name, latest_appointment_visits.timestamp FROM relationships JOIN patient_user ON (relationships.to_id = patient_user.id) LEFT JOIN latest_appointment_visits ON (latest_appointment_visits.patient_id = patient_user.id)";
@@ -70,5 +73,27 @@ public class DoctorPatientViewDAO extends DAO {
 		}
 		
 		return allViews;
+	}
+	
+	public String findDefaultDoctorByPatientId(int id){
+		try {
+			// get connection
+		    Statement stmt = ds.getStatement();
+			ResultSet rs = stmt.executeQuery("SELECT from_id as doctor_id FROM relationships WHERE to_id=" + id + " AND rel_type='DEFAULT_DOCTOR'");
+			
+			UserDAO userDAO = new UserDAO();
+			User user = new User();;
+			while(rs.next()){
+				user = userDAO.findById(rs.getInt(1));
+			}
+			return ("Dr. " + user.getFirstName().toString() + " " + user.getLastName().toString());
+		} catch (NullPointerException e){
+			return "No Doctor Assigned";
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+		    ds.close();
+		}
 	}
 }
