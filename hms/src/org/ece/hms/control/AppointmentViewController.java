2f4f32b46de24935cb582b7b3b7aa1a6aa538b69
Craig Gurnik <cgurnik@gmail.com>
2012-04-01 21:08:41 -0400

    basic validation on appointments

--- hms/src/org/ece/hms/control/AppointmentViewController.java
+++ hms/src/org/ece/hms/control/AppointmentViewController.java
@@ -18,6 +18,7 @@ import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Grid;
+import org.zkoss.zul.Intbox;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Textbox;
@@ -25,7 +26,8 @@ import org.zkoss.zul.Timebox;
 import org.zkoss.zul.Window;
 
 public class AppointmentViewController extends GenericForwardComposer<Window> {
-	private Textbox doctorId, patientId, length;
+	private Textbox doctorId, patientId;
+	private Intbox length;
 	private Datebox date;
 	private Timebox time;
 	private Label mesgLbl;
@@ -39,7 +41,7 @@ public class AppointmentViewController extends GenericForwardComposer<Window> {
 					.get("appointment");
 			doctorId.setValue(Integer.toString(appointment.getDoctorId()));
 			patientId.setValue(Integer.toString(appointment.getPatientId()));
-			length.setValue(Integer.toString(appointment.getAppointmentLength()));
+			length.setValue(appointment.getAppointmentLength());
 			date.setValue(appointment.getDate());
 			time.setValue(appointment.getDate());
 		}
@@ -86,7 +88,7 @@ public class AppointmentViewController extends GenericForwardComposer<Window> {
 			date.setHours(time.getValue().getHours());
 			date.setMinutes(time.getValue().getMinutes());
 			appt.setDate(new Timestamp(date.getTime()));
-			appt.setLength(Integer.valueOf(length.getValue()));
+			appt.setLength(length.getValue());
 			AppointmentDAO apptDAO = new AppointmentDAO();
 
 			if (arg.containsKey("appointment")) {
